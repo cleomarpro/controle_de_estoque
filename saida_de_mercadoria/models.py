a560edfc4bfbce5f9549c682e6bc0ec2472e8dff
cleomarpro <68112981+cleomarpro@users.noreply.github.com>
2022-06-20 14:58:56 -0300

    Foi removindo data de validade do models SaidaMercadoria

--- saida_de_mercadoria/models.py
+++ saida_de_mercadoria/models.py
@@ -18,6 +18,5 @@ class SaidaMercadoria(models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     quantidade = models.DecimalField(max_digits=9, decimal_places=2, blank=False, default=1)
     data_hora = models.DateTimeField(default=timezone.now)
-    validade_produto = models.DateField(blank=True, null=True)
     user = models.CharField(max_length=100, blank=True, null=True)
     usuarios = models.ForeignKey(Usuarios, null=True, on_delete=models.CASCADE)
